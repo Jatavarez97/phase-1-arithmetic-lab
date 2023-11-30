2e6499ff1db2a199829a9671c7d405d1ff4bb9d4
Jacqueline Tavarez <Jatavarez97@gmail.com>
2023-11-29 21:55:14 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = num1
+
+const num3 = 16
+const num4 = 4
+
+const mod = num3 / num4
+
+const max = (4, 8, 12, 16, 20)
